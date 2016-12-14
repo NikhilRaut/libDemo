804cbacfc6d1e4f4771342ecde25d4b7095331d9
NikhilRautGladMinds <nikhil.raut@gladminds.co>
2016-12-14 09:46:36 +0530

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/demo.java
@@ -0,0 +1,17 @@
+package com.example.mylibrary;
+
+/**
+ * Created by Nikhil on 14-12-2016.
+ */
+
+public class demo {
+    String name;
+
+    public demo() {
+
+    }
+
+    public demo(String name) {
+        this.name = name;
+    }
+}
